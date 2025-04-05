314f2e3400e1b311dfdd075134296531eaa2b029
Nazar Pasichnyk <nazarpas2007@gmail.com>
2025-04-05 21:43:22 +0300

    feat: update Users model structure

--- server/apps/authentication/models.py
+++ server/apps/authentication/models.py
@@ -1,8 +1,16 @@
 from sqlmodel import SQLModel, Field
-from typing import Optional
+from uuid import uuid4, UUID
+from typing import Optional, List
+from enum import Enum
+
+class Role(str, Enum):
+    USER = "user"
+    ADMIN = "admin"
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    first_name: str = Field(index=True, unique=False, max_length=50)
+    last_name: str = Field(index=True, unique=False, max_length=50)
     email: str = Field(index=True, unique=True)
     hashed_password: str
+    roles: List[Role] = Field(default=[Role.USER])

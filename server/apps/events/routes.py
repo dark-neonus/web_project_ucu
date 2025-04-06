68250aaab4958053942db7a8c0767decb53c97a2
Nazar Pasichnyk <nazarpas2007@gmail.com>
2025-04-06 18:09:39 +0300

    fix: fix broken routs

--- server/apps/events/routes.py
+++ server/apps/events/routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
 from server.core.database import get_db
 from .models import Event
 from .schemas import EventCreate, EventResponse
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from pathlib import Path
 
 router = APIRouter()
@@ -29,14 +29,21 @@ def list_events(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return events
 
 @router.get("/events", response_class=HTMLResponse)
-def events_page():
+def events_page(content: str = Query("events"), db: Session = Depends(get_db)):
+    # Redirect to /events?content=events if the query parameter 'content' is missing
+    # if content is None:
+    #     return RedirectResponse(url="/events/events/?content=events")
+
+    # Check if the query parameter 'content' is equal to 'events'
+    if content != "events":
+        raise HTTPException(status_code=400, detail="Invalid query parameter")
+
     # Path to the registration HTML file
-    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/forum-page.html"
-    # html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/forum-page.html?content=events"
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/events-page.html"
 
     # Read the HTML file content
     if html_file_path.exists():
         html_content = html_file_path.read_text(encoding="utf-8")
         return HTMLResponse(content=html_content)
     else:
-        return HTMLResponse(content="<h1>Registration page not found</h1>", status_code=404)
+        return HTMLResponse(content="<h1>Events page not found</h1>", status_code=404)

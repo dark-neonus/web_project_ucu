3c14df5bdd64e6620cf2871fafa4a80b4fe5aa8a
Nazar Pasichnyk <nazarpas2007@gmail.com>
2025-04-06 20:40:20 +0300

    refactor: reroute events and forum pages

--- server/apps/events/routes.py
+++ server/apps/events/routes.py
@@ -8,38 +8,16 @@ from pathlib import Path
 
 router = APIRouter()
 
-@router.post("/events/", response_model=EventResponse)
-def create_event(event: EventCreate, db: Session = Depends(get_db)):
-    db_event = Event(**event.dict())
-    db.add(db_event)
-    db.commit()
-    db.refresh(db_event)
-    return db_event
 
-@router.get("/events/{event_id}", response_model=EventResponse)
-def read_event(event_id: int, db: Session = Depends(get_db)):
-    event = db.query(Event).filter(Event.id == event_id).first()
-    if event is None:
-        raise HTTPException(status_code=404, detail="Event not found")
-    return event
+# @router.get("/events/", response_model=list[EventResponse])
+# def list_events(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+#     events = db.query(Event).offset(skip).limit(limit).all()
+#     return events
 
-@router.get("/events/", response_model=list[EventResponse])
-def list_events(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    events = db.query(Event).offset(skip).limit(limit).all()
-    return events
-
-@router.get("/events", response_class=HTMLResponse)
-def events_page(content: str = Query("events"), db: Session = Depends(get_db)):
-    # Redirect to /events?content=events if the query parameter 'content' is missing
-    # if content is None:
-    #     return RedirectResponse(url="/events/events/?content=events")
-
-    # Check if the query parameter 'content' is equal to 'events'
-    if content != "events":
-        raise HTTPException(status_code=400, detail="Invalid query parameter")
-
+@router.get("/", response_class=HTMLResponse)
+def events_page():
     # Path to the registration HTML file
-    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/events-page.html"
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/forum-events-page.html"
 
     # Read the HTML file content
     if html_file_path.exists():
@@ -47,3 +25,31 @@ def events_page(content: str = Query("events"), db: Session = Depends(get_db)):
         return HTMLResponse(content=html_content)
     else:
         return HTMLResponse(content="<h1>Events page not found</h1>", status_code=404)
+
+@router.get("/create_event", response_class=HTMLResponse)
+def create_event_page():
+    # Path to the registration HTML file
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/create-event-page.html"
+
+    # Read the HTML file content
+    if html_file_path.exists():
+        html_content = html_file_path.read_text(encoding="utf-8")
+        return HTMLResponse(content=html_content)
+    else:
+        return HTMLResponse(content="<h1>Create event page not found</h1>", status_code=404)
+
+
+@router.get("/view_event/{event_id}", response_class=HTMLResponse)
+def view_event_page(event_id: int, db: Session = Depends(get_db)):
+    # event = db.query(Event).filter(Event.id == event_id).first()
+    # if event is None:
+    #     raise HTTPException(status_code=404, detail="Event not found")
+    # return event
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/event-view-page.html"
+
+    # Read the HTML file content
+    if html_file_path.exists():
+        html_content = html_file_path.read_text(encoding="utf-8")
+        return HTMLResponse(content=html_content)
+    else:
+        return HTMLResponse(content="<h1>View event page not found</h1>", status_code=404)
--- server/apps/forum/routes.py
+++ server/apps/forum/routes.py
@@ -9,42 +9,38 @@ from .schemas import PostCreate, PostResponse
 
 router = APIRouter()
 
-@router.post("/posts/", response_model=PostResponse)
-def create_post(post: PostCreate, db: Session = Depends(get_db)):
-    db_post = Post(**post.dict())
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-    return db_post
-
-@router.get("/posts/{post_id}", response_model=PostResponse)
-def read_post(post_id: int, db: Session = Depends(get_db)):
-    post = db.query(Post).filter(Post.id == post_id).first()
-    if post is None:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return post
-
-# @router.get("/posts/", response_model=list[PostResponse])
-# def read_posts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-#     posts = db.query(Post).offset(skip).limit(limit).all()
-#     return posts
-
-@router.get("/posts", response_class=HTMLResponse)
-def events_page(content: str = Query("questions"), db: Session = Depends(get_db)):
-    # Redirect to /events?content=events if the query parameter 'content' is missing
-    # if content is None:
-    #     return RedirectResponse(url="/forum/posts/?content=forum")
-
-    # Check if the query parameter 'content' is equal to 'events'
-    if content != "questions":
-        raise HTTPException(status_code=400, detail="Invalid query parameter")
-
+@router.get("/", response_class=HTMLResponse)
+def events_page():
     # Path to the registration HTML file
-    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/forum-page.html"
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/forum-posts-page.html"
 
     # Read the HTML file content
     if html_file_path.exists():
         html_content = html_file_path.read_text(encoding="utf-8")
         return HTMLResponse(content=html_content)
     else:
-        return HTMLResponse(content="<h1>Questions page not found</h1>", status_code=404)
+        return HTMLResponse(content="<h1>Forum page not found</h1>", status_code=404)
+
+@router.get("/create_post", response_class=HTMLResponse)
+def create_post_page():
+    # Path to the registration HTML file
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/create-post-page.html"
+
+    # Read the HTML file content
+    if html_file_path.exists():
+        html_content = html_file_path.read_text(encoding="utf-8")
+        return HTMLResponse(content=html_content)
+    else:
+        return HTMLResponse(content="<h1>Create post page not found</h1>", status_code=404)
+
+
+@router.get("/view_post/{post_id}", response_class=HTMLResponse)
+def view_post_page(post_id: int, db: Session = Depends(get_db)):
+    html_file_path = Path(__file__).parent.parent.parent.parent / "src/pages/post-view-page.html"
+
+    # Read the HTML file content
+    if html_file_path.exists():
+        html_content = html_file_path.read_text(encoding="utf-8")
+        return HTMLResponse(content=html_content)
+    else:
+        return HTMLResponse(content="<h1>View post page not found</h1>", status_code=404)
